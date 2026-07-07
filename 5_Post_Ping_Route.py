323cdae046e09f706fca063d59fd672075fe5601
SalmanBilalOfficials <137156683+Salman-Bilal@users.noreply.github.com>
2026-07-07 18:53:30 +0500

    Upload Tasks of Day 1 of Week 2

---
+++ 5_Post_Ping_Route.py
@@ -0,0 +1,13 @@
+"""
+Question: 5: Add a POST /ping route that returns status code 201 with {"status": "created"}.
+
+"""
+
+from fastapi import FastAPI, status
+
+app = FastAPI()
+
+@app.post("/ping", status_code=status.HTTP_201_CREATED)
+def ping_server():
+
+    return {"status" : "created"}
